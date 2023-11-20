9b92ec689bc7fb6fd27a425574dea9669e015226
Mohsin Raza <zmohsinraza64@gmail.com>
2023-11-20 11:55:24 -0800

    managing state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,64 @@
-function App() {
+import { useReducer } from "react";
+import AddTask from "./Components/AddTask";
+import TaskList from "./Components/Managingstate";
 
-  return (
+export default function TaskAdd(){
+  const [tasks, dispatch] = useReducer(
+    tasksReducer, initialTasks
+  );
+
+  function handleAddTask(text){
+    dispatch({type: 'added', id: nextId++, text: text,});
+  }
+
+  function handleChangeTask(task){
+    dispatch({type: 'changed', task: task});
+  }
+
+  function handleDeleteTask(taskId){
+    dispatch({type: 'deleted', id: taskId});
+  }
+
+  return(
     <>
-    <h1>React</h1>
+      <h1>List Todo</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList tasks={tasks} onChangeTask={handleChangeTask} onDeleteTask={handleDeleteTask} />
     </>
   );
+
 }
 
-export default App
+function tasksReducer(tasks, action){
+  switch (action.type){
+    case 'added': {
+      return [...tasks, {
+        id: action.id,
+        text: action.text,
+        done: false
+      }];
+    }
+    case 'changed' :{
+      return tasks.map(t => {
+        if (t.id === action.task.id){
+          return action.task;
+        }else{
+          return t;
+        }
+      });
+    }
+    case 'deleted': {
+      return tasks.filter(t => t.id !== action.id);
+    }
+    default:{
+      throw Error('Unknown action: ' + action.type);
+    }
+  }
+}
+
+let nextId = 3;
+const initialTasks = [
+  {id: 0, text: 'Pakistan', done: true},
+  {id: 1, text: 'Karachi', done: false},
+  {id: 2, text: 'Nawabshah', done: false}
+];
